74a08af72550263dba4af936f61ae1abe0fc5734
Jason Horvath <jasonhorvath@jasons-mbp.myfiosgateway.com>
2022-05-21 15:10:09 -0400

    multi return handler example

--- server/server.go
+++ server/server.go
@@ -5,8 +5,12 @@ import (
 	"net/http"
 )
 
+type Handler func(w http.ResponseWriter, r *http.Request)
+
 func ServerStart() {
 	http.HandleFunc("/", index)
+	uri, handler := multiReturnHandler()
+	http.HandleFunc(uri, handler)
 	fmt.Println("The server is listening. Go to http://localhost:3500 in your browser")
 	_ = http.ListenAndServe(":3500", nil)
 }
@@ -17,4 +21,18 @@ func index(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(err)
 	}
 	fmt.Println(fmt.Sprintf("Number of bytes in the response content: %d", content))
+}
+
+func multiReturnHandler() (string, Handler) {
+	uri := "/multireturnhandler"
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		content, err := fmt.Fprintf(w, "This is the multi return handler in the server package.")
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(fmt.Sprintf("Number of bytes in the response content: %d", content))
+	}
+
+	return uri, handler
 }

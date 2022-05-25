5d834b90000e2224233973e33815a9a71fa4f3be
Jason Horvath <kaingnx@gmail.com>
2022-05-25 08:39:12 -0400

    Add HtmlTemplate Method

--- render/render.go
+++ render/render.go
@@ -4,9 +4,25 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"text/template"
 
 	"github.com/jason-horvath/goexamples/schema"
 )
+
+// HtmlTemplate - Renders the template using the http response writer
+func HtmlTemplate(w http.ResponseWriter, templatePath string, templateData schema.ExampleHtmlData) {
+	w.Header().Set("Content-Type", "text/html")
+	htmlTemplate, err := template.ParseFiles(templatePath)
+	if err != nil {
+		log.Println(err)
+	}
+
+	err = htmlTemplate.Execute(w, templateData)
+
+	if err != nil {
+		panic(err)
+	}
+}
 
 // JsonOutput - Takes the AlteredJson struct and renders it
 func JsonOutput(w http.ResponseWriter, jsonToRender schema.OutputJason) {

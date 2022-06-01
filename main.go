6a73e607df0d3e1e9f815940e5bdbce2435bc17c
Jason Horvath <kaingnx@gmail.com>
2022-05-31 21:40:53 -0400

    use app config

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "github.com/jason-horvath/goexamples/server"
+import (
+	"log"
+
+	"github.com/jason-horvath/goexamples/config"
+	"github.com/jason-horvath/goexamples/render"
+	"github.com/jason-horvath/goexamples/server"
+)
 
 func main() {
+	var app config.AppConfig
+	tmplCache, err := render.BuildTemplateCache()
+	if err != nil {
+		log.Println("Unable to load the template cache", err)
+	}
+	app.TemplateCache = tmplCache
 	server.ServerStart()
 }

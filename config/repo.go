d406edf56cec82b5d8e4aab35251ff84fce4ea19
Jason Horvath <kaingnx@gmail.com>
2022-06-08 19:29:54 -0400

    Add comments

--- config/repo.go
+++ config/repo.go
@@ -6,12 +6,14 @@ type Repository struct {
 	App *AppConfig
 }
 
+// CreateRepo - returns a new repository for configuration
 func CreateRepo(a *AppConfig) *Repository {
 	return &Repository{
 		App: a,
 	}
 }
 
+// SetRepo - Takes a Repository and sets it to Repo
 func SetRepo(r *Repository) {
 	Repo = r
 }

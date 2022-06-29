6b0eb1267f5ee0f85afea7a861b3829887c13989
Jason Horvath <kaingnx@gmail.com>
2022-06-29 08:27:38 -0400

    router package idea

--- router/routes.go
+++ router/routes.go
@@ -1,5 +1,26 @@
 package router
 
+import "github.com/jason-horvath/goexamples/handlers"
+
+type Route struct {
+	Uri     string
+	Handler handlers.Handler
+}
+
+type RouteCollection struct {
+	GET    map[string]Route
+	POST   map[string]Route
+	PUT    map[string]Route
+	PATCH  map[string]Route
+	DELETE map[string]Route
+}
+
 func Routes() {
 
 }
+
+func (rc *RouteCollection) SetRoute(httpType string, route Route) {
+
+}
+
+// Still hashing out the ideas on this. Just saving here.
